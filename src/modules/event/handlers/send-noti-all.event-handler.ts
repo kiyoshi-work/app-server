9d02119f925f0e919d5877739330aae3e8f52dab
0xTardis <hungnt17.fyt@gmail.com>
2023-11-17 17:11:27 +0700

    UPDATE save notiall

--- src/modules/event/handlers/send-noti-all.event-handler.ts
+++ src/modules/event/handlers/send-noti-all.event-handler.ts
@@ -10,6 +10,8 @@ import {
 import { SendNotiAllEvent } from '../impls/sent-noti-all.event';
 import { User } from '@/modules/onesignal/http/v1/user';
 import { In } from 'typeorm';
+import { chunk } from '@/shared/utils';
+import { MAX_EXTERNAL_IDS_SEND_NOTIFICATION } from '@/modules/onesignal/constant';
 
 @EventsHandler(SendNotiAllEvent)
 export class SendNotiAllEventHandler
@@ -48,23 +50,20 @@ export class SendNotiAllEventHandler
         ...data,
         type: event.type,
       });
-      let _currCount = 0;
-      const _currOffset = 0;
-      while (true) {
-        const response: any = await this.oneSignalUser.listUsers({
-          appId: event?.onesignalAppId,
-          apiKey: event?.onesignalApiKey,
-          limit: 200,
-          offset: _currOffset,
-        });
-        const _tmpUser = new Set();
-        (response.players || []).map((player) =>
-          _tmpUser.add(player.external_user_id),
-        );
+      const externalIds: any = await this.oneSignalUser.getAllExternalIds({
+        appId: event?.onesignalAppId,
+        apiKey: event?.onesignalApiKey,
+        limit: 200,
+      });
+      const externalIdsChunked = chunk(
+        externalIds,
+        MAX_EXTERNAL_IDS_SEND_NOTIFICATION,
+      );
+      for (const _externalIds of externalIdsChunked) {
         const users = await this.userRepository.find({
           where: {
             client_id: event.client_id,
-            client_uid: In([..._tmpUser]),
+            client_uid: In(_externalIds),
           },
           select: ['id'],
         });
@@ -77,13 +76,46 @@ export class SendNotiAllEventHandler
             })),
           );
         } catch (error) {
-          console.log('send noti all error: ', error);
-        }
-        _currCount += Number(response.limit);
-        if (_currCount >= Number(response.total_count || 0)) {
-          break;
+          console.log('save noti all error: ', error);
         }
       }
+
+      // let _currCount = 0;
+      // const _currOffset = 0;
+      // while (true) {
+      //   const response: any = await this.oneSignalUser.listUsers({
+      //     appId: event?.onesignalAppId,
+      //     apiKey: event?.onesignalApiKey,
+      //     limit: 200,
+      //     offset: _currOffset,
+      //   });
+      //   const _tmpUser = new Set();
+      //   (response.players || []).map((player) =>
+      //     _tmpUser.add(player.external_user_id),
+      //   );
+      //   const users = await this.userRepository.find({
+      //     where: {
+      //       client_id: event.client_id,
+      //       client_uid: In([..._tmpUser]),
+      //     },
+      //     select: ['id'],
+      //   });
+      //   try {
+      //     await this.userNotificationRepository.logToDatabase(
+      //       users.map((_receiver) => ({
+      //         receiverId: _receiver.id,
+      //         notificationId: notification?.id,
+      //         status: ENotificationStatus.Sent,
+      //       })),
+      //     );
+      //   } catch (error) {
+      //     console.log('send noti all error: ', error);
+      //   }
+      //   _currCount += Number(response.limit);
+      //   if (_currCount >= Number(response.total_count || 0)) {
+      //     break;
+      //   }
+      // }
     }
   }
 }
