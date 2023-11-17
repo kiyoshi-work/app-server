b26da15de542f945f6b7029e0639c6d6959a865d
0xTardis <hungnt17.fyt@gmail.com>
2023-11-17 15:29:22 +0700

    UPDATE sendall

--- src/modules/onesignal/http/v1/user.ts
+++ src/modules/onesignal/http/v1/user.ts
@@ -4,6 +4,7 @@ import {
   IBodyCreateUser,
   IParamsAddTags,
   IParamsCreateUser,
+  IParamsGetUsers,
   IResponseCreateUser,
   IResponseEditTags,
   ITags,
@@ -11,11 +12,20 @@ import {
 import { generateTagKeyByUser } from '@/onesignal/util';
 import { Injectable } from '@nestjs/common';
 import { HttpClient } from '../clients/http-client';
+import axios, { AxiosRequestConfig } from 'axios';
 
 @Injectable()
 export class User extends HttpClient {
   protected endpoint(): string {
     return '/users';
+  }
+  protected getClientHeaders(apiKey?: string): Record<string, string> {
+    return {
+      'Content-Type': 'application/json; charset=utf-8',
+      Authorization: `Basic ${
+        apiKey ? apiKey : process.env.ONESIGNAL_REST_API_KEY
+      }`,
+    };
   }
 
   async create(params: IParamsCreateUser): Promise<IResponseCreateUser> {
@@ -40,6 +50,50 @@ export class User extends HttpClient {
 
     return response.data;
   }
+  sendRequest = async (options: AxiosRequestConfig) => {
+    try {
+      const response = await axios.request(options);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  async listUsers(params: IParamsGetUsers): Promise<IResponseCreateUser> {
+    try {
+      const options = {
+        method: 'GET',
+        url: `https://onesignal.com/api/v1/players`,
+        timeout: 1000,
+        params: {
+          app_id: params.appId,
+          limit: params.limit,
+          offset: params.offset,
+        },
+        headers: this.getClientHeaders(params.apiKey),
+      };
+      return this.sendRequest(options);
+    } catch (err) {
+      console.log('🚀 ~ file: user.ts:65 ~ User ~ listUsers ~ err:', err);
+    }
+  }
+
+  getAllExternalIds = async (params: IParamsGetUsers) => {
+    const externalIds = new Set();
+    let _currCount = 0;
+    const _getAllExternalIds = async (offset: number = 0) => {
+      const response: any = await this.listUsers({ ...params, offset });
+      const players = response.players || [];
+      players.forEach((player) => externalIds.add(player.external_user_id));
+      const totalPlayers = response.total_count || 0;
+      _currCount += Number(response.limit);
+      if (totalPlayers > _currCount) {
+        await _getAllExternalIds(offset + 1);
+      }
+    };
+    await _getAllExternalIds();
+    return [...externalIds];
+  };
 
   async addTags(params: IParamsAddTags): Promise<boolean> {
     try {
